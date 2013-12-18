c9fe740e35d2d715510a3ed86d2cdc2fe99d4519
mkolosick <kolosick.m@husky.neu.edu>
2013-12-17 19:34:40 -0500

    Moved Coqtop to it's own file

---
+++ coqtop.py
@@ -0,0 +1,46 @@
+from subprocess import Popen, PIPE, DEVNULL
+from threading import Thread
+
+try:
+    from Queue import Queue, Empty
+except ImportError:
+    from queue import Queue, Empty
+
+class Coqtop:
+    def __init__(self):
+        self.proc = Popen(['coqtop'], stdin=PIPE, stderr=DEVNULL, stdout=PIPE, universal_newlines=True)
+        self.queue = Queue()
+        self.thread = Thread(target=self.enqueue_output)
+        self.thread.daemon = True
+        self.thread.start()
+        print(self.get_lines())
+
+    def enqueue_output(self):
+        for line in iter(self.proc.stdout.readline, ''):
+            self.queue.put(line)
+
+    def get_lines(self):
+        lines = []
+        line = False
+
+        while line == False:
+            try:
+                line = self.queue.get_nowait()
+            except Empty:
+                line = False
+            else:
+                lines.append(line)
+                if not self.queue.empty():
+                    line = False
+
+        return lines
+
+    '''
+    Sends statement to coqtop
+    Only give it statements, not comments
+    Returns coqtop's output
+    '''
+    def send(self, statement):
+        self.proc.stdin.write(statement+'\n')
+        self.proc.stdin.flush()
+        return self.get_lines()
--- sublimecoq.py
+++ sublimecoq.py
@@ -1,11 +1,5 @@
-from subprocess import Popen, PIPE, DEVNULL
-from threading import Thread
 import re
-
-try:
-    from Queue import Queue, Empty
-except ImportError:
-    from queue import Queue, Empty
+import coqtop
 
 '''
 returns (the next statement, rest) or
@@ -30,45 +24,6 @@ def next_statement(program):
             return None
         else:
             return ((program[0:index+1], program[index+1:]) if index+1 < len(program) else (program[0:index+1], ''))
-
-class Coqtop:
-    def __init__(self):
-        self.proc = Popen(['coqtop'], stdin=PIPE, stderr=DEVNULL, stdout=PIPE, universal_newlines=True)
-        self.queue = Queue()
-        self.thread = Thread(target=self.enqueue_output)
-        self.thread.daemon = True
-        self.thread.start()
-        print(self.get_lines())
-
-    def enqueue_output(self):
-        for line in iter(self.proc.stdout.readline, ''):
-            self.queue.put(line)
-
-    def get_lines(self):
-        lines = []
-        line = False
-
-        while line == False:
-            try:
-                line = self.queue.get_nowait()
-            except Empty:
-                line = False
-            else:
-                lines.append(line)
-                if not self.queue.empty():
-                    line = False
-
-        return lines
-
-    '''
-    Sends statement to coqtop
-    Only give it statements, not comments
-    Returns coqtop's output
-    '''
-    def send(self, statement):
-        self.proc.stdin.write(statement+'\n')
-        self.proc.stdin.flush()
-        return self.get_lines()
 
 if __name__ == '__main__':
     f = open('Basics.v', 'r')
