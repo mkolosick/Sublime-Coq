d34d9a3d1fe46f4f00c65b5827d2ea9f89ff4051
mkolosick <kolosick.m@husky.neu.edu>
2014-01-02 12:42:06 -0500

    Commands can be run from *COQTOP* file now

--- sublimecoq.py
+++ sublimecoq.py
@@ -5,86 +5,108 @@ from SublimeCoq.coqtop import Coqtop
 
 class CoqNextStatementCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        current_position = self.view.settings().get('current_position')
-        current_position = self.view.find('\\s*', current_position).end()
+        if self.view.name() == '*COQTOP*':
+            coqfile_name = self.view.settings().get('coqfile_name')
+            coqfile_group = self.view.settings().get('coqfile_group')
+            views = self.view.window().views_in_group(coqfile_group)
+            for v in views:
+                if v.file_name() == coqfile_name:
+                    coqfile_view = v
+                    break
+        else:
+            coqfile_view = self.view
+        
+        current_position = coqfile_view.settings().get('current_position')
+        current_position = coqfile_view.find('\\s*', current_position).end()
 
-        indicator = self.view.substr(current_position) + self.view.substr(current_position + 1)
+        indicator = coqfile_view.substr(current_position) + coqfile_view.substr(current_position + 1)
 
         if indicator == '(*':
-            r = self.view.find('\\(\\*(.|\\n)*?\\*\\)', current_position)
-            current_comment_number = self.view.settings().get('current_comment_number')
+            r = coqfile_view.find('\\(\\*(.|\\n)*?\\*\\)', current_position)
+            current_comment_number = coqfile_view.settings().get('current_comment_number')
             name = 'comment: ' + repr(current_comment_number)
-            self.view.settings().set('current_comment_number', current_comment_number + 1)
+            coqfile_view.settings().set('current_comment_number', current_comment_number + 1)
         else:
-            r = self.view.find('(.|\\n)*?\\.', current_position)
-            text = self.view.substr(r)
+            r = coqfile_view.find('(.|\\n)*?\\.', current_position)
+            text = coqfile_view.substr(r)
 
-            if self.view.scope_name(current_position) == 'source.coq keyword.source.coq ':
+            if coqfile_view.scope_name(current_position) == 'source.coq keyword.source.coq ':
                 if text == 'Proof.':
-                    self.view.settings().set('proof_mode', True)
+                    coqfile_view.settings().set('proof_mode', True)
 
-            if self.view.settings().get('proof_mode'):
+            if coqfile_view.settings().get('proof_mode'):
                 if text == 'Qed.' or text == 'Admitted.' or text == 'Save.' or text == 'Defined.':
-                    self.view.settings().set('proof_mode', False)
-                current_proof_number = self.view.settings().get('current_proof_number')
+                    coqfile_view.settings().set('proof_mode', False)
+                current_proof_number = coqfile_view.settings().get('current_proof_number')
                 name = 'proof: ' + repr(current_proof_number)
-                self.view.settings().set('current_proof_number', current_proof_number + 1)
+                coqfile_view.settings().set('current_proof_number', current_proof_number + 1)
             else:
-                current_statement_number = self.view.settings().get('current_statement_number')
+                current_statement_number = coqfile_view.settings().get('current_statement_number')
                 name = 'statement: ' + repr(current_statement_number)
-                self.view.settings().set('current_statement_number', current_statement_number + 1)
+                coqfile_view.settings().set('current_statement_number', current_statement_number + 1)
             
-        self.view.show(r)
-        self.view.settings().set('current_position', r.end())
-        self.view.add_regions(name, [r], 'comment')
+        coqfile_view.show(r)
+        coqfile_view.settings().set('current_position', r.end())
+        coqfile_view.add_regions(name, [r], 'comment')
 
 class CoqUndoStatementCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        if self.view.settings().get('proof_mode'):
-            previous_proof_number = self.view.settings().get('current_proof_number') - 1
-            previous_region = self.view.get_regions('proof: ' + repr(previous_proof_number))[0]
-            if self.view.substr(previous_region) == 'Proof.':
+        if self.view.name() == '*COQTOP*':
+            coqfile_name = self.view.settings().get('coqfile_name')
+            coqfile_group = self.view.settings().get('coqfile_group')
+            views = self.view.window().views_in_group(coqfile_group)
+            for v in views:
+                if v.file_name() == coqfile_name:
+                    coqfile_view = v
+                    break
+        else:
+            coqfile_view = self.view
+
+        if coqfile_view.settings().get('proof_mode'):
+            previous_proof_number = coqfile_view.settings().get('current_proof_number') - 1
+            previous_region = coqfile_view.get_regions('proof: ' + repr(previous_proof_number))[0]
+            if coqfile_view.substr(previous_region) == 'Proof.':
                 print('Abort and resend.')
-                self.view.settings().set('proof_mode', False)
+                coqfile_view.settings().set('proof_mode', False)
             else:
                 print('Undo.')
-            self.view.settings().set('current_proof_number', previous_proof_number)
-            self.view.erase_regions('proof: ' + repr(previous_proof_number))
+            coqfile_view.settings().set('current_proof_number', previous_proof_number)
+            coqfile_view.erase_regions('proof: ' + repr(previous_proof_number))
         else:
             no_comment = False
             no_statement = False
             try:
-                previous_comment_number = self.view.settings().get('current_comment_number') - 1
-                previous_comment_region = self.view.get_regions('comment: ' + repr(previous_comment_number))[0]
+                previous_comment_number = coqfile_view.settings().get('current_comment_number') - 1
+                previous_comment_region = coqfile_view.get_regions('comment: ' + repr(previous_comment_number))[0]
             except IndexError:
                 no_comment = True
             try:
-                previous_statement_number = self.view.settings().get('current_statement_number') - 1
-                previous_statement_region = self.view.get_regions('statement: ' + repr(previous_statement_number))[0]
+                previous_statement_number = coqfile_view.settings().get('current_statement_number') - 1
+                previous_statement_region = coqfile_view.get_regions('statement: ' + repr(previous_statement_number))[0]
             except IndexError:
                 no_statement = True
             if no_statement or (not no_comment and previous_comment_region.begin() > previous_statement_region.begin()):
                 previous_region = previous_comment_region
-                self.view.erase_regions('comment: ' + repr(previous_comment_number))
-                self.view.settings().set('current_comment_number', previous_comment_number)
+                coqfile_view.erase_regions('comment: ' + repr(previous_comment_number))
+                coqfile_view.settings().set('current_comment_number', previous_comment_number)
             else:
                 previous_region = previous_statement_region
-                name = self.view.substr(self.view.word(self.view.word(previous_region.begin()).end() + 1))
+                name = coqfile_view.substr(coqfile_view.word(coqfile_view.word(previous_region.begin()).end() + 1))
                 print('Reset ' + name)
                 while True:
-                    previous_proof_number = self.view.settings().get('current_proof_number') - 1
+                    previous_proof_number = coqfile_view.settings().get('current_proof_number') - 1
                     if previous_proof_number == -1:
                         break
                     else:
-                        previous_proof_region = self.view.get_regions('proof: ' + repr(previous_proof_number))[0]
+                        previous_proof_region = coqfile_view.get_regions('proof: ' + repr(previous_proof_number))[0]
                         if previous_proof_region.begin() < previous_region.begin():
                             break
                         else:
-                            self.view.erase_regions('proof: ' + repr(previous_proof_number))
-                            self.view.settings().set('current_proof_number', previous_proof_number)
-                self.view.settings().set('current_statement_number', previous_statement_number)
-                self.view.erase_regions('statement: ' + repr(previous_statement_number))
-        self.view.settings().set('current_position', previous_region.begin())
+                            coqfile_view.erase_regions('proof: ' + repr(previous_proof_number))
+                            coqfile_view.settings().set('current_proof_number', previous_proof_number)
+                coqfile_view.settings().set('current_statement_number', previous_statement_number)
+                coqfile_view.erase_regions('statement: ' + repr(previous_statement_number))
+        coqfile_view.settings().set('current_position', previous_region.begin())
 
 
 class CoqStopCommand(sublime_plugin.TextCommand):
@@ -94,7 +116,7 @@ class CoqStopCommand(sublime_plugin.TextCommand):
             coqfile_group = self.view.settings().get('coqfile_group')
             views = self.view.window().views_in_group(coqfile_group)
             for v in views:
-                if v.name() == coqfile_name:
+                if v.file_name() == coqfile_name:
                     coqfile_view = v
                     break
             self.view.window().run_command('close')
@@ -143,7 +165,8 @@ class RunCoqCommand(sublime_plugin.TextCommand):
         coqtop_view.set_read_only(True)
         coqtop_view.set_scratch(True)
         coqtop_view.settings().set('coqtop_running', True)
-        coqtop_view.settings().set('coqfile_name', self.view.name())
+        coqtop_view.settings().set('coqfile_name', self.view.file_name())
+        print(self.view.file_name())
 
         coqtop_view.settings().set('coqfile_group', editor_group)
         self.view.settings().set('coqtop_group', coq_group)
